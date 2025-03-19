f6bb54eb753db32a9be095b31f3c9851eb7f5470
Rahul Kurmalkar <therahulkurmalkar@gmail.com>
2025-03-20 04:38:36 +0530

    removed_config.ts

--- frontend/src/Hooks.ts
+++ frontend/src/Hooks.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { BACKEND_URL } from "./config";
 import { useEffect, useState } from "react";
 
 export interface BlogInterface {
--- frontend/src/config.ts
+++
@@ -1,5 +0,0 @@
-/* production */
-export const BACKEND_URL="https://backend.therahulkurmalkar.workers.dev";
-
-/* local */
-// export const BACKEND_URL="http://localhost:8787";
